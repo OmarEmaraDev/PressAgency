e395f991b489304212f8bcba319c72c0e62d3f83
Omar Emara <mail@OmarEmara.dev>
2021-05-19 20:25:14 +0200

    Initial implementation

---
+++ Controllers/WallController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PressAgency.Controllers {
+  public class WallController : Controller {
+    public WallController() {}
+
+    public IActionResult Index() { return View(); }
+  }
+}
