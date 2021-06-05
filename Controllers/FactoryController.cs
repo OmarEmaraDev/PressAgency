f82bd96849fee4fb233d29ca233f7f926a5f3caa
Omar Emara <mail@OmarEmara.dev>
2021-06-05 22:59:34 +0200

    Add Factory layout
    Co-authored-by: NadaGamal1 <82242287+NadaGamal1@users.noreply.github.com>

---
+++ Controllers/FactoryController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authorization;
+
+namespace PressAgency.Controllers {
+  public class FactoryController : Controller {
+    public FactoryController() {}
+
+    [AllowAnonymous]
+    public IActionResult Index() { return View(); }
+
+    [AllowAnonymous]
+    public IActionResult LoginModal() { return Redirect("/#loginModal"); }
+
+    [Authorize(Policy = "ViewerOnly")]
+    public IActionResult Saved() { return View(); }
+  }
+}
